d39080018d75cfcdd33e6438b4a27dc3081137d5
Ben Altman <altmanguitar@gmail.com>
2022-11-03 00:01:00 -0400

    completed triangle_project_2

--- triangle.rb
+++ triangle.rb
@@ -15,7 +15,20 @@
 #
 def triangle(a, b, c)
   # WRITE THIS CODE
-  
+  a, b, c = [a, b, c].sort
+
+  # initial exceptions:
+  # if a <= 0 || b <= 0 || c <= 0
+  #   raise TriangleError, "Triangle sides must be greater than zero" 
+  # elsif a + b <= c
+  #   raise TriangleError, "Violates the triangle inequality theorem"
+  # end
+
+  # second exceptions:
+  if a + b <= c
+    raise TriangleError, "not a valid triangle"
+  end
+
   if a == b && a == c
     :equilateral
   elsif a == b || a == c || b == c
@@ -23,6 +36,13 @@ def triangle(a, b, c)
   else
     :scalene
   end  
+
+  # prettier answer from stackexchange:
+  # case [a,b,c].uniq.size
+  #   when 3; :scalene
+  #   when 2; :isosceles
+  #   when 1; :equilateral
+  # end
 end
 
 # Error class used in part 2.  No need to change this code.
